648c76b000ba7391c4012181f515f49fce6694a1
Norali Gorosito <norali84g@gmail.com>
2026-05-16 20:45:35 -0300

    feat: mostrar información real del usuario y cerrar sesión - closes #16

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule],
+  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css'
 })
 export class Settings {
-  notificationsEnabled = true;
-  darkModeEnabled = false;
+  constructor(private authService: AuthService) { }
+
+  getUser() {
+    return this.authService.getUser();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
